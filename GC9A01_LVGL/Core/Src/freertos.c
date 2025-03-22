1d8e826161df89bd16247ae6ab0ce4a23437dced
Ben <ben.grummitt@outlook.com>
2025-03-22 21:48:43 +0000

    feat: use spinner for example

--- GC9A01_LVGL/Core/Src/freertos.c
+++ GC9A01_LVGL/Core/Src/freertos.c
@@ -156,13 +156,11 @@ void StartTickTask(void const * argument)
   lv_display_set_flush_cb(display1, GC9A01_flush);
 
   /*Change the active screen's background color*/
-  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_hex(0x003a57), LV_PART_MAIN);
+  lv_obj_set_style_bg_color(lv_screen_active(), lv_color_white(), LV_PART_MAIN);
 
-  /*Create a white label, set its text and align it to the center*/
-  lv_obj_t * label = lv_label_create(lv_screen_active());
-  lv_label_set_text(label, "Hello world");
-  lv_obj_set_style_text_color(lv_screen_active(), lv_color_hex(0xffffff), LV_PART_MAIN);
-  lv_obj_align(label, LV_ALIGN_CENTER, 0, 0);
+  lv_obj_t * preload = lv_spinner_create(lv_scr_act());
+  lv_obj_set_size(preload, 100, 100);
+  lv_obj_align(preload, LV_ALIGN_CENTER, 0, 0);
 
   /* Infinite loop */
   for(;;)
